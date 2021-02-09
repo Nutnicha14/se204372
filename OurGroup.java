e7d23adc27a6a39df23cf21bb0f680c58b88a660
Nutnicha14 <78755196+Nutnicha14@users.noreply.github.com>
2021-02-09 23:55:00 +0700

    Add files via upload

--- OurGroup.java
+++ OurGroup.java
@@ -6,10 +6,14 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
+        groupMembers.add("Nongnapat Dumrongchaipak"); //PUT YOUR NAME HERE
+
         groupMembers.add("Nutnicha Phasuk"); //PUT YOUR NAME HERE
+
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
     }
+  
 
 }
