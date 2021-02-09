51d19435df5241fb3c9f0b6cd1556b0c036dca9c
Nutnicha14 <nutnicha.pha@ku.th>
2021-02-09 23:02:15 +0700

    My first group changes to OurGroup.java

---
+++ OurGroup.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class OurGroup {
+
+    private Vector<String> groupMembers;
+    public OurGroup()
+    {
+        groupMembers = new Vector<String>();
+        groupMembers.add("Nutnicha Phasuk"); //PUT YOUR NAME HERE
+    }
+    public Vector<String> getGroupMembers () {
+        return groupMembers;
+    }
+
+}
